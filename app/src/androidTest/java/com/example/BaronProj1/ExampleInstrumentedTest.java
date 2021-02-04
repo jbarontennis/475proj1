cd05d2177255e1645226ae7b909e5bd68512c118
James Baron <james.baron.17@cnu.edu>
2021-02-03 21:53:19 -0500

    basically done

--- app/src/androidTest/java/com/example/BaronProj1/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/BaronProj1/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.myapplication3;
+package com.example.BaronProj1;
 
 import android.content.Context;
 
--- app/src/main/java/com/example/BaronProj1/MainActivity.java
+++ app/src/main/java/com/example/BaronProj1/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.myapplication3;
+package com.example.BaronProj1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
